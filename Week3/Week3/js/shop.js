5706e7a178036c23a777c0b78bc1e11dcb6d9c38
DESKTOP-BJQS2KO\pavin <pavin.rao@students.stamford.edu>
2016-12-02 10:09:09 +0700

    no message

---
+++ Week3/Week3/js/shop.js
@@ -0,0 +1,10 @@
+var customerName = "Pavin";
+var price = 10;
+var quantity = 2;
+var totalPrice = price * quantity;
+
+var customerElement = document.getElementById("customer");
+customerElement.textContent = customerName;
+
+var customerElement = document.getElementById("price");
+customerElement.textContent = price;
