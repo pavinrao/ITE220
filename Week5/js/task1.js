4293f1f8ff071421f898cbb1660eb5c7f874dcc5
DESKTOP-BJQS2KO\pavin <pavin.rao@students.stamford.edu>
2017-02-17 18:59:31 +0700

    no message

--- Week5/js/task1.js
+++ Week5/js/task1.js
@@ -14,8 +14,7 @@ var task1 = {
 	devInfo: function(){
 		var btnDev = document.getElementById("dev-info");
 		btnDev.addEventListener("click", function(){
-			document.getElementById("dev-info-container").innerhtml = 
-			"<p>Pavin</p><p>Student</p><p><a href='https://github.com/pavinrao' target='_blank'>https://github.com/pavinrao</a></p>";
+
 		});
 	}
 }
